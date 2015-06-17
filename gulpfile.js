a99be798cd69d6e69584772ddf21aa382d0e0ebc
aevtushenko <aevtushenko@xiag.ch>
2015-06-17 13:09:35 +0600

    Change dir structure.

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ var gulp = require('gulp');
 gulp.task('less', function () {
     var less = require('gulp-less');
 
-    return gulp.src('css/*.less')
+    return gulp.src(__dirname + '/src/css/*.less')
         .pipe(less())
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest(__dirname + '/dist/css'));
 });
 
 gulp.task('watch', ['less'], function () {
-    gulp.watch('css/*.less', ['less']);
+    gulp.watch(__dirname + '/src/css/*.less', ['less']);
 });
 
 gulp.task('server', ['watch'], function () {
