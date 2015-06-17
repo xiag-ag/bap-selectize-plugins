d2b8323143e156b0df1a9073479cc541008bd191
aevtushenko <aevtushenko@xiag.ch>
2015-06-17 13:09:35 +0600

    Add multiselect example.

--- example/js/main.js
+++ example/js/main.js
@@ -9,3 +9,14 @@ $('select[name="C"]').selectize({
     selectOnTab: true,
     placeholder: '- select -'
 });
+$('select[name="D"]').selectize({
+    plugins: ['caret_button'],
+    valueField: 'id',
+    labelField: 'title',
+    searchField: 'title',
+    maxItems: 3,
+    openOnFocus: false,
+    createOnBlur: true,
+    selectOnTab: true,
+    placeholder: '- select -'
+});
